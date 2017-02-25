ac2b2d2bca10fd9d72f489e003e65e1a44a42533
Alex <Alex@PC-VANMECHELEN>
2017-02-25 16:37:38 +0100

    - warnings  --> kicking users (could be changed to ban) - code = more spreaded

--- MimmiBot/Core.cs
+++ MimmiBot/Core.cs
@@ -12,9 +12,11 @@ namespace MimmiBot
     internal class Core
     {
         List<string> Admins = new List<string>();
+        List<string> UsersToKick = new List<string>();
         private readonly DiscordClient _bot;
         private readonly CommandService _commands;
         public ulong OutroChannelId;
+        private string userId;
 
         private enum Command
         {
@@ -60,51 +62,54 @@ namespace MimmiBot
 
         public void Bot_UserJoined(object sender, UserEventArgs e)
         {
-
+            userId =  "<@" + e.User.Id + ">";
+            Setwarnings(userId, 10);
         }
 
         public void Bot_UserLeft(object sender, UserEventArgs e)
         {
-            var logChannel = e.Server.GetChannel(OutroChannelId);
-            logChannel.SendMessage($"{e.User.Name} has left the magical Ember of Dreams (Gaming Community) Server. :sleepy: ");
+            try
+            {
+                var logChannel = e.Server.GetChannel(OutroChannelId);
+                logChannel.SendMessage($"{e.User.Name} has left the magical Ember of Dreams (Gaming Community) Server. :sleepy: ");
+            }
+            catch
+            {
+                Console.WriteLine("You should install an outro channel with the \".setoutro\" command");
+            }
         }
 
         public void Bot_MessageReceived(object sender, MessageEventArgs e)
         {
-            string currentuser = e.User.Name;
-            Random rand = new Random();
-            int messagetosend;
-            messagetosend = rand.Next(0, 3);
-            var isUserAdmin = false;
-            var str = e.Message.RawText.Split(Convert.ToChar(" "));
-            foreach (var role in e.User.Roles)
+            userId = "@" + e.User.Name + "#" + e.User.Discriminator;
+            Setwarnings(userId, 10);
+            int messagetosend, newPoints;
+            bool isUserAdmin;
+            string[] str;
+            Setup(e, out messagetosend, out isUserAdmin, out str);
+            if (UsersToKick.Contains("<@" + e.User.Id + ">"))
             {
-                if (role.ToString() == "Ember Keeper")
+                e.User.Kick();
+                UsersToKick.Remove("<@" + e.User.Id + ">");
+                try
                 {
-                    isUserAdmin = true;
+                    var logChannel = e.Server.GetChannel(OutroChannelId);
+                    logChannel.SendMessage($"{e.User.Name} has been auto kicked from the magical Ember of Dreams (Gaming Community) Server.");
+                }
+                catch
+                {
+                    Console.WriteLine("You should install an outro channel with the \".setoutro\" command");
                 }
             }
 
-            bool Contains = false;
-            Contains = Admins.Any(s => e.User.Name.Contains(s));
-            if (Contains)
+            else if ((e.Message.RawText.ToLower().StartsWith(".ae") && str.Length == 3 && isUserAdmin) || (e.Message.RawText.ToLower().StartsWith(".ae") && str.Length == 3 && Admins.Contains("@" + e.User.Name + "#" + e.User.Discriminator)))
             {
-                isUserAdmin = true;
-            }
-            int newPoints;
-            if ((e.Message.RawText.ToLower().StartsWith(".ae") && str.Length == 3 && isUserAdmin) || (e.Message.RawText.ToLower().StartsWith(".ae") && str.Length == 3 && Admins.Contains(str[Convert.ToInt32(Command.Uid)])))
-            {
-                var userId = str[Convert.ToInt32(Command.Uid)].Replace("<@!", "<@");
+                userId = str[Convert.ToInt32(Command.Uid)];
                 var pointsToAdd = Convert.ToInt32(str[Convert.ToInt32(Command.Points)]);
                 newPoints = CalcNewpoints(GetPoints(userId), Convert.ToInt32(str[Convert.ToInt32(Command.Points)]), true);
                 if (pointsToAdd == 0)
                 {
-                    if (messagetosend == 0)
-                        e.Channel.SendMessage("We thought it was implied... but it needs to be a number greater than 0 ^.^");
-                    else if (messagetosend == 1)
-                        e.Channel.SendMessage("Are you a hacher? Please do not try to break my code!");
-                    else if (messagetosend == 2)
-                        e.Channel.SendMessage("Please do not make me feel that I'm useless. :cry: ");
+                    Send0message(e, messagetosend);
                 }
                 else
                 {
@@ -116,22 +121,15 @@ namespace MimmiBot
                 }
             }
 
-            else if ((e.Message.RawText.ToLower().StartsWith(".re") && str.Length == 3 && isUserAdmin) || (e.Message.RawText.ToLower().StartsWith(".re") && str.Length == 3 && Admins.Contains(str[Convert.ToInt32(Command.Uid)])))
+            else if ((e.Message.RawText.ToLower().StartsWith(".re") && str.Length == 3 && isUserAdmin) || (e.Message.RawText.ToLower().StartsWith(".re") && str.Length == 3 && Admins.Contains("@" + e.User.Name + "#" + e.User.Discriminator)))
             {
                 Random rand_ = new Random();
-                int messagetosend_;
-                messagetosend_ = rand_.Next(0, 3);
-                var userId = str[Convert.ToInt32(Command.Uid)].Replace("<@!", "<@");
+                userId = str[Convert.ToInt32(Command.Uid)];
                 var pointsToAdd = Convert.ToInt32(str[Convert.ToInt32(Command.Points)]);
                 newPoints = CalcNewpoints(GetPoints(userId), Convert.ToInt32(str[Convert.ToInt32(Command.Points)]), false);
                 if (pointsToAdd == 0)
                 {
-                    if (messagetosend_ == 0)
-                        e.Channel.SendMessage("We thought it was implied... but it needs to be a number greater than 0 ^.^");
-                    else if (messagetosend_ == 1)
-                        e.Channel.SendMessage("Are you a hacher? Please do not try to break my code!");
-                    else if (messagetosend == 2)
-                        e.Channel.SendMessage("Please do not make me feel that I'm useless. :cry: ");
+                    Send0message(e, messagetosend);
                 }
                 else
                 {
@@ -155,43 +153,9 @@ namespace MimmiBot
                 }
             }
 
-            else if (e.Message.RawText.ToLower().StartsWith(".ae"))
-            {
-                string userId;
-                if ((str.Length > 1 && isUserAdmin) || (str.Length > 1 && Admins.Contains(str[Convert.ToInt32(Command.Uid)].Replace("<@!", "<@"))))
-                {
-                    userId = str[1];
-                }
-                else if (str.Length == 1)
-                {
-                    userId = e.User.NicknameMention;
-                }
-                else
-                {
-                    return;
-                }
-
-                if (isUserAdmin)
-                    e.Channel.SendMessage(userId + " Type just \".ae\" in the chat to add embers.");
-                else
-                    e.Channel.SendMessage(userId + " You need to have the 'Admin' rank to use this command.");
-            }
-
             else if (e.Message.RawText.ToLower().StartsWith(".refresh"))
             {
-                string userId;
-                if ((str.Length > 1 && isUserAdmin) || (str.Length > 1 && Admins.Contains(str[Convert.ToInt32(Command.Uid)])))
-                {
-                    userId = str[1];
-                }
-                else if (str.Length == 1)
-                {
-                    userId = e.User.NicknameMention;
-                }
-                else
-                {
-                    return;
-                }
+                userId = "@" + e.User.Name + "#" + e.User.Discriminator;
                 if (!Admins.Contains(userId) && isUserAdmin)
                 {
                     Admins.Add(userId);
@@ -206,69 +170,98 @@ namespace MimmiBot
                 }
             }
 
-            else if ((e.Message.RawText.StartsWith(".remove") && isUserAdmin) || (e.Message.RawText.StartsWith(".remove") && Admins.Contains(str[Convert.ToInt32(Command.Uid)])))
+            else if ((e.Message.RawText.StartsWith(".remove") && isUserAdmin) || (e.Message.RawText.StartsWith(".remove") && Admins.Contains("@" + e.User.Name + "#" + e.User.Discriminator)))
             {
-                var userId = str[Convert.ToInt32(Command.Uid)];
+                userId = str[Convert.ToInt32(Command.Uid)];
                 if (Admins.Contains(userId))
                 {
                     Admins.Remove(userId);
                     e.Channel.SendMessage("Succesfully removed " + userId + " from the Admin list.");
                 }
-                else if (isUserAdmin || Admins.Contains(userId))
+                else if (isUserAdmin || Admins.Contains("<@" + e.User.Id.ToString() + ">"))
                     e.Channel.SendMessage("That user is not an Admin for me.");
                 else
                     e.Channel.SendMessage("You need to be an Admin to use this command. Try '.refresh' if you are.");
             }
 
-            else if (e.Message.RawText.ToLower().StartsWith(".re"))
-            {
-                string userId;
-                if ((str.Length > 1 && isUserAdmin) || (str.Length > 1 && Admins.Contains(str[Convert.ToInt32(Command.Uid)].Replace("<@!", "<@"))))
-                {
-                    userId = str[1];
-                }
-                else if (str.Length == 1)
-                {
-                    userId = e.User.NicknameMention;
-                }
-                else
-                {
-                    return;
-                }
-
-                if (isUserAdmin)
-                    e.Channel.SendMessage(userId + " Type just \".re\" in the chat to remove embers.");
-                else
-                    e.Channel.SendMessage(userId + " You need to have the 'Admin' rank to use this command.");
-            }
-
             else if (e.Message.RawText.ToLower().StartsWith(".embers"))
             {
-                string userId;
-                if ((str.Length > 1 && isUserAdmin) || (str.Length > 1 && Admins.Contains(str[Convert.ToInt32(Command.Uid)].Replace("<@!", "<@"))))
-                {
-                    userId = str[1];
-                }
-                else if (str.Length == 1)
-                {
-                    userId = e.User.NicknameMention;
-                }
-                else
-                {
-                    return;
-                }
-
-                userId = userId.Replace("<@!", "<@");
-                e.Channel.SendMessage(userId + " Your current points balance is " + GetPoints(userId));
+                userId = "@" + e.User.Name + "#" + e.User.Discriminator;
+                e.Channel.SendMessage(e.User.Mention + " Your current points balance is " + GetPoints(userId));
             }
 
-            else if ((e.Message.RawText.ToLower().StartsWith(".setoutro") && isUserAdmin) || (e.Message.RawText.ToLower().StartsWith(".setoutro") && Admins.Contains(str[Convert.ToInt32(Command.Uid)])))
+            else if (e.Message.RawText.ToLower().StartsWith(".warnings"))
+            {
+                userId = "<@" + e.User.Id + ">";
+                var warnings = 10 - Getwarnings(userId);
+                if (warnings == 1)
+                    e.Channel.SendMessage(e.User.Mention + " You have been warned " + warnings + " time!");
+                else
+                    e.Channel.SendMessage(e.User.Mention + " You have been warned " + warnings + " times!");
+            }
+
+            else if (e.Message.RawText.ToLower().StartsWith(".setoutro") && isUserAdmin)
             {
                 OutroChannelId = e.Channel.Id;
-                var Outrochannel = e.Server.GetChannel(OutroChannelId);
-                e.Channel.SendMessage("#" + Outrochannel + " is the new outro channel.");
+                e.Channel.SendMessage("#" + e.Server.GetChannel(OutroChannelId) + " is the new outro channel.");
             }
-         
+
+            else if ((e.Message.RawText.StartsWith(".warn") && isUserAdmin) || (e.Message.RawText.StartsWith(".warn") && Admins.Contains("@" + e.User.Name + "#" + e.User.Discriminator)))
+            {
+                userId = str[Convert.ToInt32(Command.Uid)];
+                try
+                {
+                    Getwarnings(userId);
+                }
+                catch
+                {
+                    Setwarnings(userId, 10);
+                }
+
+                if (!(Getwarnings(userId) == 0))
+                    Setwarnings(userId, Getwarnings(userId) - 1);
+                else
+                    Setwarnings(userId, 0);
+                Console.WriteLine(Getwarnings(userId));
+                if (Getwarnings(userId) == 0)
+                {
+                    if (!UsersToKick.Contains(userId))
+                        UsersToKick.Add(userId);
+                }
+            }
+        }
+
+        private void Setup(MessageEventArgs e, out int messagetosend, out bool isUserAdmin, out string[] str)
+        {
+            string currentuser = e.User.Name;
+            Random rand = new Random();
+            messagetosend = rand.Next(0, 3);
+            isUserAdmin = false;
+            str = e.Message.RawText.Split(Convert.ToChar(" "));
+            foreach (var role in e.User.Roles)
+            {
+                if (role.ToString() == "Ember Keeper")
+                {
+                    isUserAdmin = true;
+                }
+            }
+
+            bool Contains = false;
+            Contains = Admins.Any(s => e.User.Name.Contains(s));
+            if (Contains)
+            {
+                isUserAdmin = true;
+            }
+        }
+
+        private static void Send0message(MessageEventArgs e, int messagetosend)
+        {
+            if (messagetosend == 0)
+                e.Channel.SendMessage("We thought it was implied... but it needs to be a number greater than 0 ^.^");
+            else if (messagetosend == 1)
+                e.Channel.SendMessage("Are you a hacher? Please do not try to break my code!");
+            else if (messagetosend == 2)
+                e.Channel.SendMessage("Please do not make me feel that I'm useless. :cry: ");
         }
 
         private void DeleteLastMessage()
@@ -327,6 +320,34 @@ namespace MimmiBot
             return 0;
         }
 
+        private int Getwarnings(string uid)
+        {
+            try
+            {
+                if (!Directory.Exists("Data/")) Directory.CreateDirectory("Data/");
+                if (!File.Exists("Data/Warnings.txt"))
+                {
+                    using (var sw = new StreamWriter("Data/Warnings.txt"))
+                    {
+                    }
+                }
+                var lineArr = File.ReadAllLines("Data/Warnings.txt");
+
+                foreach (var line in lineArr)
+                {
+                    var userAttributes = line.Split(Convert.ToChar(";"));
+                    if (userAttributes[0] != uid) continue;
+                    return Convert.ToInt32(userAttributes[1]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return 0;
+        }
+
         private void SetPoints(string uid, int points)
         {
             if (!Directory.Exists("Data/")) Directory.CreateDirectory("Data/");
@@ -362,6 +383,44 @@ namespace MimmiBot
             {
                 lines.Add(uid + ";" + points);
                 File.WriteAllLines("data/points.txt", lines);
+            }
+        }
+
+        private void Setwarnings(string uid, int Warnings)
+        {
+            if (!Directory.Exists("Data/")) Directory.CreateDirectory("Data/");
+            if (!File.Exists("Data/Warnings.txt"))
+            {
+                using (var sw = new StreamWriter("Data/Warnings.txt"))
+                {
+
+                }
+            }
+            var lines = File.ReadAllLines("Data/Warnings.txt").ToList();
+
+            var lineFound = false;
+
+            foreach (var line in lines)
+            {
+                if (!line.Contains(uid)) continue;
+                var lineArr = lines.ToArray();
+                var lineNumber = Array.FindIndex(lineArr, row => row.Contains(uid));
+                if (line.Contains(uid))
+                {
+                    lineArr[lineNumber] = uid + ";" + Warnings;
+                }
+                lines = lineArr.ToList();
+                lineFound = true;
+            }
+
+            if (lineFound)
+            {
+                File.WriteAllLines("data/Warnings.txt", lines);
+            }
+            else
+            {
+                lines.Add(uid + ";" + 10);
+                File.WriteAllLines("data/Warnings.txt", lines);
             }
         }
 
